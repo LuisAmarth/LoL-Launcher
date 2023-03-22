472287bf2d8a46cd9a4b7413a0ee3547178d47e4
Amarth <105468138+LuisAmarth@users.noreply.github.com>
2023-03-21 21:30:29 -0300

    primeira versão

---
+++ script.js
@@ -0,0 +1,11 @@
+var sound1 = new Audio()
+    sound1.src = "/sound/play.mp3"
+
+var sound2 = new Audio()
+    sound2.src = "/sound/mousehover.mp3"
+
+var sound3 = new Audio()
+    sound3.src = "sound/navclick.mp3"
+
+var sound4 = new Audio()
+    sound4.src = "sound/mousehover2.mp3"
